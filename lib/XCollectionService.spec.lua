f4dc25eb27a1558f3c4aad621a24412f8cda00cd
Noah Crowley <noahwillcrow@gmail.com>
2019-02-19 09:31:41 -0500

    Set up unit test system

--- lib/XCollectionService.spec.lua
+++ lib/XCollectionService.spec.lua
@@ -1,3 +1,15 @@
 return function()
-    
+    if pcall(function() game:GetService("CollectionService") end) then
+        local XCSModule = require(script.Parent.XCollectionService)
+        local XCollectionService = XCSModule.GetInstance()
+
+        it("should only create one instance", function()
+            expect(XCSModule.GetInstance()).to.equal(XCollectionService)
+        end)
+    else
+        print("No CollectionService, not running dependent tests")
+        it("should error without CollectionService", function()
+            expect(function() game:GetService("CollectionService") end).to.throw()
+        end)
+    end
 end
